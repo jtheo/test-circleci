ddf23c5db20d7c8dbd0966339b1ce6c77edcd6cc
Matteo Marchelli <matteo.marchelli@your.md>
2018-02-07 12:57:08 +0000

    Background jobs, test nr 11

--- report.sh
+++ report.sh
@@ -3,7 +3,7 @@
 . include.sh
 
 t=0
-for l in ${LOG_DIR}
+for l in ${LOG_DIR}/*
 do 
   work_time=$(grep "^# Sleep timing:" $l | sed 's/# Sleep timing: .* - Sum: \([0-9]*\)/\1/')
   t=$(( ${t} + ${work_time} ))
