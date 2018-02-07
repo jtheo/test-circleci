8f1c111a5f7d8b74e1d2dc815441185753160ac0
Matteo Marchelli <matteo.marchelli@your.md>
2018-02-07 13:33:59 +0000

    Background jobs, test nr 15

--- dummy.sh
+++ dummy.sh
@@ -2,18 +2,17 @@
 # Dummy script
 
 . include.sh
-# trap "kill 0" EXIT
-# trap "exit" INT TERM ERR
-
 
 mkdir -p ${LOG_DIR}
 
 for step in dev prod stg infra legacy
 do
-  Trap_err=$(( ( $RANDOM % 10 )  + 1 ))
+  Trap_err=$(( ( ($RANDOM * $RANDOM) % 10 )  + 1 ))
   msg "Launching Step ${step}: ${Trap_err}"
   bash sleeper.sh ${Trap_err} > ${LOG_DIR}/${step}.log &
 done
 
 msg "Jobs:\n$(jobs -l)"
+msg "Waiting for completion..."
 wait
+msg "Done!"
